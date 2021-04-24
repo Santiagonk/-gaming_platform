f46508f57ba0a5a9cf22f4a93ac9bc3a118d01b3
SantiagoNK <jssalgadog@gmail.com>
2021-04-24 16:05:51 -0500

    Getting started with chat example

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
+
+// proof code
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+// });
+
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
