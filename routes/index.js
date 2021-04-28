b74e75b9bb3cb87ca7713e9908ed305d2796bbdd
Santiago Salgado <jssalgadog@gmail.com>
2021-04-28 11:22:37 -0500

    changes to sign in and sign up

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const router = Router();
-const { getUsers, createUser, getUsersById, updatedUser,  deleteUser} = require('../controllers/controller')
+const { getUsers, createUser, getUsersById, updatedUser,  deleteUser, getUsersByUsername} = require('../controllers/controller')
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
 router.post('/users', createUser);
 router.put('/users/:id', updatedUser);
 router.delete('/users/:id', deleteUser);
+
+router.post('/sign-up', createUser);
+router.get('/sign-in/:username', getUsersByUsername);
 
 module.exports = router;
